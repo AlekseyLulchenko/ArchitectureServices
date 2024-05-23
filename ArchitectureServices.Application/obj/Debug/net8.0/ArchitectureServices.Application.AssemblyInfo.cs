efb3d848cc93bcd69d740d465bfadab754b6a5c5
a.lyulchenko <a.lyulchenko@ase-ec.ru>
2024-05-23 17:29:21 +0300

    gitignore

--- ArchitectureServices.Application/obj/Debug/net8.0/ArchitectureServices.Application.AssemblyInfo.cs
+++ ArchitectureServices.Application/obj/Debug/net8.0/ArchitectureServices.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchitectureServices.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3a14e0731fcc0d623a8c3a7d7f54400eef4fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchitectureServices.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchitectureServices.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ArchitectureServices.Core/obj/Debug/net8.0/ArchitectureServices.Core.AssemblyInfo.cs
+++ ArchitectureServices.Core/obj/Debug/net8.0/ArchitectureServices.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchitectureServices.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3a14e0731fcc0d623a8c3a7d7f54400eef4fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchitectureServices.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchitectureServices.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
